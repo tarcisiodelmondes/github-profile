f226ae32d5635a0839c840d31bf91338c58a448b
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-04 14:19:31 -0300

    Refactor: home page redone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,21 @@
 import Image from "next/image";
-import { useContext } from "react";
-import { Loading } from "../components/Loading";
-import { ProfileFound } from "../components/Profile/ProfileFound";
-import { ProfileNotFound } from "../components/Profile/ProfileNotFound";
-import { LoadingContext } from "../contexts/LoadingContext";
-import { ProfileContext } from "../contexts/ProfileContext";
-
+import { Search } from "../components/Search";
 import styles from "./home.module.scss";
 
 export default function Home() {
-  const { profile, hasError } = useContext(ProfileContext);
-
-  const { isLoading } = useContext(LoadingContext);
-
   return (
-    <div className={styles.homepage}>
-      <Loading isLoading={isLoading} />
-      {!profile && !hasError ? (
-        <div className={styles.github__image}>
-          <Image
-            width={250}
-            height={250}
-            src="/github.png"
-            alt="Image of github"
-          />
-        </div>
-      ) : hasError ? (
-        <ProfileNotFound />
-      ) : (
-        <ProfileFound />
-      )}
-    </div>
+    <main className={styles.homepage}>
+      <Image
+        width={250}
+        height={250}
+        src="/github.png"
+        alt="Image of github"
+        objectFit="cover"
+        className={styles.img}
+      />
+      <div className={styles.containerSearch}>
+        <Search />
+      </div>
+    </main>
   );
 }
